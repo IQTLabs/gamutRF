577ca95769a50dd4ecda55bd67c95f14306b4a67
Josh Bailey <josh@vandervecken.com>
2022-05-19 04:40:11 +0000

    more coverage.

--- tests/test_sdr_recorder.py
+++ tests/test_sdr_recorder.py
@@ -19,14 +19,23 @@ class SDRRecorderTestCase(unittest.TestCase):
     def test_sdr_recorder(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             sdr_recorder = TestRecorder()
+            record_status, sample_file = sdr_recorder.run_recording(tmpdir, 1e3, 1e3, 1e3, 0, False, 0, sigmf_=False)
+            self.assertTrue(os.path.exists(sample_file))
+            sdr_recorder.tmpdir.cleanup()
+        with tempfile.TemporaryDirectory() as tmpdir:
+            sdr_recorder = TestRecorder()
             # TODO: sigmf 1.0.0 can't parse .zst files, but it can write the metadata fine.
             record_status, sample_file = sdr_recorder.run_recording(tmpdir, 1e3, 1e3, 1e3, 0, False, 0, sigmf_=True)
             self.assertTrue(os.path.exists(sample_file))
             self.assertGreater(os.path.getsize(sample_file), 0)
             self.assertTrue(os.path.exists(sample_file + '.sigmf-meta'))
-            self.assertNotEqual(None, sdr_recorder.validate_request([], 1e6, 0, 0))
-            self.assertEqual(None, sdr_recorder.validate_request([], 1e6, 1e6, 1e6))
             sdr_recorder.tmpdir.cleanup()
+
+        sdr_recorder = TestRecorder()
+        self.assertNotEqual(None, sdr_recorder.validate_request([], 1e6, 0, 0))
+        self.assertNotEqual(None, sdr_recorder.validate_request([], 1e6, 1, 1))
+        self.assertEqual(None, sdr_recorder.validate_request([], 1e6, 1e6, 1e6))
+        sdr_recorder.tmpdir.cleanup()
 
 
 if __name__ == '__main__':
