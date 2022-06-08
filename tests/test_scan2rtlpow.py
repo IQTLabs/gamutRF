c2feedb75d207793f6d68c3c97b299740d1bcfea
cglewis <clewis@iqt.org>
2022-06-08 14:18:30 -0700

    use scan

--- tests/test_scan2rtlpow.py
+++ tests/test_scan2rtlpow.py
@@ -3,5 +3,5 @@ from gamutrf import scan2rtlpow
 
 
 def test_scan2rtlpow_main():
-    sys.argv = [sys.argv[0], 'tests/data/verybusy1g1.csv', 'out.csv']
+    sys.argv = [sys.argv[0], 'tests/data/scan.csv', 'out.csv']
     scan2rtlpow.main()
