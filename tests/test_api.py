c31c7cb4c2aa37807e325bdc690b13096e015517
cglewis <clewis@iqt.org>
2022-06-08 13:34:22 -0700

    add more tests

---
+++ tests/test_api.py
@@ -0,0 +1,1 @@
+from gamutrf import api
---
+++ tests/test_scan2rtlpow.py
@@ -0,0 +1,7 @@
+import sys
+from gamutrf import scan2rtlpow
+
+
+def test_scan2rtlpow_main():
+    sys.argv = [sys.argv[0], 'data/verybusy1g1.csv', 'out.csv']
+    scan2rtlpow.main()
