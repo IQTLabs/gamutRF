ae65ca339090410426b8eaa555ba09aaf0431e6c
cglewis <clewis@iqt.org>
2022-06-08 13:48:20 -0700

    fix path

--- tests/test_scan2rtlpow.py
+++ tests/test_scan2rtlpow.py
@@ -3,5 +3,5 @@ from gamutrf import scan2rtlpow
 
 
 def test_scan2rtlpow_main():
-    sys.argv = [sys.argv[0], 'data/verybusy1g1.csv', 'out.csv']
+    sys.argv = [sys.argv[0], 'tests/data/verybusy1g1.csv', 'out.csv']
     scan2rtlpow.main()
