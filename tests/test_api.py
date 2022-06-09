0ca579e22863ad9fa4f7174f3a0091887ca90b12
cglewis <clewis@iqt.org>
2022-06-08 17:51:09 -0700

    fix imports

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,15 +5,12 @@ import pytest
 
 sys.argv = [sys.argv[0]]
 from gamutrf import api
-from gamutrf.api.API import create_app
 
 
 @pytest.fixture(scope='module')
 def client():
-    return testing.TestClient(create_app())
-
-def test_app():
     app = api.API(start_app=False)
+    return testing.TestClient(app.create_app())
 
 def test_routes(client):
     result = client.simulate_get('/v1')
