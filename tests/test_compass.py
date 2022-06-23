54fd5d4b81e64d03dd7c9139c6e89a073fa996b5
cglewis <clewis@iqt.org>
2022-06-23 11:16:31 -0700

    update test for name change

--- tests/test_compass.py
+++ tests/test_compass.py
@@ -6,6 +6,6 @@ sys.modules['smbus2'] = fake_rpi.smbus
 from gamutrf import compass
 
 
-def test_compass_bearing():
-    bearing = compass.Bearing()
-    bearing.get_bearing()
+def test_compass_heading():
+    heading = compass.Heading()
+    heading.get_heading()
