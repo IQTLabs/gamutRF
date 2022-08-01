5dd650d8da11270f58f97b6042f2b60e195674ad
cglewis <clewis@iqt.org>
2022-08-01 12:43:35 -0700

    fix tests

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -1,3 +1,4 @@
+import pytest
 import sys
 
 from gamutrf import scan
@@ -12,25 +13,37 @@ def test_scan_argument_parser():
 
 
 def test_bad_freq():
-    sys.argv.append(["--freq-start", "100", "--freq-end", "99"])
-    scan.main()
+    sys.argv.append(["--freq-start=100", "--freq-end=99"])
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        scan.main()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
 
 
 def test_bad_rollover():
-    sys.argv.append(["--freq-start", "99", "--freq-end", "100"])
-    scan.main()
+    sys.argv.append(["--freq-start=99", "--freq-end=100"])
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        scan.main()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
 
 
 def test_bad_freq_end():
-    sys.argv.append(["--freq-end", "100000000000"])
-    scan.main()
+    sys.argv.append(["--freq-end=100000000000"])
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        scan.main()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
 
 
 def test_bad_freq_start():
-    sys.argv.append(["--freq-start", "10000"])
-    scan.main()
+    sys.argv.append(["--freq-start=10000"])
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        scan.main()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
 
 
 def test_scan_main():
-    sys.argv.append(["--updatetimeout", "-1"])
+    sys.argv.append(["--updatetimeout=-1"])
     scan.main()
