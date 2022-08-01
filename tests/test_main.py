6c7a1c80f462297201b33afc81639520270d7a61
cglewis <clewis@iqt.org>
2022-08-01 10:56:02 -0700

    catch system exit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 import sys
 
 from gamutrf.__main__ import api
@@ -12,11 +13,50 @@ from gamutrf.__main__ import specgram
 sys.argv.append("-h")
 
 
-def test_main():
-    api() 
-    freqxlator()
-    samples2raw()
-    scan()
-    scan2mp4()
-    sigfinder()
-    specgram()
+def test_main_api():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        api()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_main_freqxlator():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        freqxlator()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_main_samples2raw():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        samples2raw()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_main_scan():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        scan()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_main_scan2mp4():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        scan2mp4()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_main_sigfinder():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        sigfinder()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
+
+
+def test_main_specgram():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        specgram()
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 0
