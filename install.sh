43b724c9149ae769ed326e6f40aed869921d8b7e
cglewis <clewis@iqt.org>
2022-08-30 14:48:02 -0700

    typo

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ curl -V > /dev/null || echo "First install curl then try again."
 
 sudo curl https://raw.githubusercontent.com/IQTLabs/gamutRF/main/bin/gamutrf --output /usr/local/bin/gamutrf
 sudo chmod +x /usr/local/bin/gamutrf
-echo -e "\n\n\n======\n\nFinished installing.\nRun 'gamutrf' to controll various GamutRF tools and services.\n\n======\n"
+echo -e "\n\n\n======\n\nFinished installing.\nRun 'gamutrf' to control various GamutRF tools and services.\n\n======\n"
