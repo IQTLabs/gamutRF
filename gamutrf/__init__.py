c15e8a11b2313468ded92e38d07828443630bd1a
Josh Bailey <josh@vandervecken.com>
2023-06-17 19:07:23 +0000

    fmt.

--- gamutrf/__init__.py
+++ gamutrf/__init__.py
@@ -1,6 +1,6 @@
 from importlib import metadata
 
 try:
-      __version__ = metadata.version("gamutrf")
+    __version__ = metadata.version("gamutrf")
 except metadata.PackageNotFoundError:
-      __version__ = "UNKNOWN"
+    __version__ = "UNKNOWN"
