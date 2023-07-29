f3ce9359e3d2b51fbc74f82c699bd221e4d5b73b
Josh Bailey <josh@vandervecken.com>
2023-07-29 05:40:51 +0000

    semgrep.

--- static/console_script.js
+++ static/console_script.js
@@ -1,13 +1,13 @@
-document.querySelectorAll(".nav-link").forEach(link => {
+document.querySelectorAll(".nav-link").forEach((link) => {
     link.addEventListener("click", function(e) {
         e.preventDefault();
         
-        document.querySelectorAll(".nav-link").forEach(navLink => {
+        document.querySelectorAll(".nav-link").forEach((navLink) => {
             navLink.classList.remove("active");
         });
         e.target.classList.add("active");
         
-        document.querySelectorAll(".tab-pane").forEach(pane => {
+        document.querySelectorAll(".tab-pane").forEach((pane) => {
             pane.classList.remove("show", "active");
         });
         document.querySelector(e.target.getAttribute("href")).classList.add("show", "active");
