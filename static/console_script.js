e23c0024e3b066397c1960b9844196a60f53e5ee
Josh Bailey <josh@vandervecken.com>
2023-07-29 05:25:26 +0000

    quote.

--- static/console_script.js
+++ static/console_script.js
@@ -1,15 +1,15 @@
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function(e) {
+document.querySelectorAll(".nav-link").forEach(link => {
+    link.addEventListener("click", function(e) {
         e.preventDefault();
         
-        document.querySelectorAll('.nav-link').forEach(navLink => {
-            navLink.classList.remove('active');
+        document.querySelectorAll(".nav-link").forEach(navLink => {
+            navLink.classList.remove("active");
         });
-        e.target.classList.add('active');
+        e.target.classList.add("active");
         
-        document.querySelectorAll('.tab-pane').forEach(pane => {
-            pane.classList.remove('show', 'active');
+        document.querySelectorAll(".tab-pane").forEach(pane => {
+            pane.classList.remove("show", "active");
         });
-        document.querySelector(e.target.getAttribute('href')).classList.add('show', 'active');
+        document.querySelector(e.target.getAttribute("href")).classList.add("show", "active");
     });
 });
