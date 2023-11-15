398ba7c932eab6eba9252a4cb6d9e3982fac154c
Josh Bailey <josh@vandervecken.com>
2023-11-15 02:38:30 +0000

    move to MAR requirements.txt pattern.

--- torchserve/install-torchserve.sh
+++ torchserve/install-torchserve.sh
@@ -10,7 +10,6 @@ pip config set global.no-cache-dir false && \
   cd serve && \
   python3 ./ts_scripts/install_dependencies.py --environment prod $* && \
   pip3 install . && \
-  pip3 install -r examples/object_detector/yolo/yolov8/requirements.txt && \
   cd .. && \
   rm -rf serve
 
