0fabfa40dcbd0c2a8d08884019cd5907e170baa8
Luke Berndt <lukekb@gmail.com>
2024-01-26 11:28:41 -0500

    Update scan.py

--- gamutrf/scan.py
+++ gamutrf/scan.py
@@ -240,7 +240,7 @@ def argument_parser():
         help="Use dc_block_cc long form",
     )
     parser.add_argument(
-        "--disable_ettus_auto_offset",
+        "--disable_ettus_dc_offset",
         dest="dc_ettus_auto_offset",
         action="store_false",
         help="Use Ettus DC Auto offset",
