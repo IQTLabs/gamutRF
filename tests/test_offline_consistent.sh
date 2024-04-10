8da4047b8bbaac5f7d2b0b754e243cd9201fcd50
Josh Bailey <josh@vandervecken.com>
2024-04-10 05:08:51 +0000

    use consistent args

--- tests/test_offline_consistent.sh
+++ tests/test_offline_consistent.sh
@@ -12,12 +12,18 @@ python -c "import numpy as np ; x = (np.random.uniform(-16384,16383,(20480000,))
 rm -rf "$TMPDIR/ref"
 mkdir "$TMPDIR/ref"
 
-docker run -v "$TMPDIR:/gamutrf/data" -t iqtlabs/gamutrf gamutrf-offline --tune-step-fft=512 --db_clamp_floor=-150 --nfft=1024 --rotate_secs=0 "--inference_output_dir=/gamutrf/data/ref" "/gamutrf/data/input/$TESTFILE"
+run_offline()
+{
+    dir=$1
+    docker run -v "$TMPDIR:/gamutrf/data" -t iqtlabs/gamutrf gamutrf-offline --tune-step-fft=512 --db_clamp_floor=-150 --nfft=1024 --rotate_secs=0 --inference_output_dir=/gamutrf/data/"$dir" /gamutrf/data/input/$TESTFILE
+}
+
+run_offline ref
 
 for trial in {1..5} ; do
   rm -rf "$TMPDIR/trial"
   mkdir "$TMPDIR/trial"
-  docker run -v "$TMPDIR:/gamutrf/data" -t iqtlabs/gamutrf gamutrf-offline --tune-step-fft=512 --db_clamp_floor=-150 --nfft=1024 --rotate_secs=0 "--inference_output_dir=/gamutrf/data/trial" "/gamutrf/data/input/$TESTFILE"
+  run_offline trial
   for image in $TMPDIR/ref/image*png ; do
     baseimage="$(basename $image)"
     testimage="$TMPDIR/trial/$baseimage"
