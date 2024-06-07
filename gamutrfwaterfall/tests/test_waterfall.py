1a2bd761aa7288cbaca2c4034f35a3d4ba4b2142
Josh Bailey <josh@vandervecken.com>
2024-06-07 09:18:35 +0000

    Serve alternate waterfalls

--- gamutrfwaterfall/tests/test_waterfall.py
+++ gamutrfwaterfall/tests/test_waterfall.py
@@ -14,28 +14,28 @@ from gamutrflib.peak_finder import get_peak_finder
 
 
 class FakeZmqReceiver:
-    def __init__(self, run_secs, peak_min, peak_max, peak_val):
+    def __init__(self, run_secs, peak_min, peak_max, peak_val, freq_min, freq_max):
         self.start_time = time.time()
         self.run_secs = run_secs
         self.serve_results = None
         self.peak_min = peak_min
         self.peak_max = peak_max
         self.peak_val = peak_val
+        self.freq_min = freq_min
+        self.freq_max = freq_max
 
     def healthy(self):
         return time.time() - self.start_time < self.run_secs
 
-    def read_buff(self, scan_fres):
+    def read_buff(self, scan_fres=1e3):
         if not self.serve_results:
-            freq_min = 1e6
-            freq_max = 10e6
-            rows = int((freq_max - freq_min) / scan_fres)
+            rows = int((self.freq_max - self.freq_min) / scan_fres)
             df = pd.DataFrame(
                 [
                     {
                         "ts": time.time(),
-                        "freq": freq_min + (i * scan_fres),
-                        "db": self.peak_val / 2,
+                        "freq": self.freq_min + (i * scan_fres),
+                        "db": self.peak_val * 2,
                         "tune_count": i,
                     }
                     for i in range(rows)
@@ -44,7 +44,6 @@ class FakeZmqReceiver:
             df.loc[(df.freq >= self.peak_min) & (df.freq <= self.peak_max), "db"] = (
                 self.peak_val
             )
-            df["freq"] /= 1e6
             self.serve_results = [
                 (
                     [
@@ -55,6 +54,7 @@ class FakeZmqReceiver:
                             "freq_end": df.freq.max(),
                             "tune_step_hz": scan_fres,
                             "tune_step_fft": 256,
+                            "tuning_ranges": f"{int(df.freq.min())}-{int(df.freq.max())}",
                         }
                     ],
                     df,
@@ -73,15 +73,17 @@ class UtilsTestCase(unittest.TestCase):
 
     def test_run_waterfall(self):
         with tempfile.TemporaryDirectory() as tempdir:
-            peak_min = 1.50
-            peak_max = 1.51
-            peak_val = 100
+            peak_min = 1.50e6
+            peak_max = 1.52e6
+            peak_val = -10
+            freq_min = 1e6
+            freq_max = 2e6
             savefig = os.path.join(tempdir, "test.png")
-            zmqr = FakeZmqReceiver(90, peak_min, peak_max, peak_val)
+            zmqr = FakeZmqReceiver(90, peak_min, peak_max, peak_val, freq_min, freq_max)
             peak_finder = get_peak_finder("narrowband")
             serve_waterfall(
-                1e6,  # args.min_freq,
-                2e6,  # args.max_freq,
+                freq_min,  # args.min_freq,
+                freq_max,  # args.max_freq,
                 True,  # args.plot_snr,
                 1,  # args.n_detect,
                 tempdir,  # args.save_path,
@@ -117,10 +119,10 @@ class UtilsTestCase(unittest.TestCase):
                         # timestamp,start_freq,end_freq,dB,type
                         # 1.0,1.49609375,1.5078125,100.0,narrowband
                         self.assertEqual(
-                            round(peak_min, 1), round(float(row["start_freq"]), 1), row
+                            round(peak_min / 1e6, 1), round(float(row["start_freq"]), 1), row
                         )
                         self.assertEqual(
-                            round(peak_max, 1), round(float(row["end_freq"]), 1), row
+                            round(peak_max / 1e6, 1), round(float(row["end_freq"]), 1), row
                         )
                         self.assertEqual(peak_val, float(row["dB"]), row)
 
