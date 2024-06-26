47523bd0b87cfcb368fe7fa9d74b38c498dec762
Josh Bailey <josh@vandervecken.com>
2024-06-26 20:14:35 +0000

    remove specgram/waterfall samples/scipy.

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,6 @@ import sys
 
 from gamutrf.__main__ import worker
 from gamutrf.__main__ import scan
-from gamutrf.__main__ import specgram
 
 
 sys.argv.append("-h")
@@ -19,12 +18,5 @@ def test_main_worker():
 def test_main_scan():
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         scan()
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 0
-
-
-def test_main_specgram():
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        specgram()
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 0
