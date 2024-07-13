dad2ecaa2185b5060c0c338749d82533364dbd69
Josh Bailey <josh@vandervecken.com>
2024-07-13 03:27:45 +0000

    remove worker.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,10 @@
 import pytest
 import sys
 
-from gamutrf.__main__ import worker
 from gamutrf.__main__ import scan
 
 
 sys.argv.append("-h")
-
-
-def test_main_worker():
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        worker()
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 0
 
 
 def test_main_scan():
